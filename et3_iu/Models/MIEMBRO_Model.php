ace57ccb6ab386b1e00b47ac4adda7ec4e827c4d
Raquel Marcos <raquelmarcosenr@gmail.com>
2016-12-28 11:52:05 +0100

    Apellido2 borrado

--- et3_iu/Models/MIEMBRO_Model.php
+++ et3_iu/Models/MIEMBRO_Model.php
@@ -3,8 +3,7 @@ class Miembro_Model{
 
     private $DNI;
     private $NOMBRE;
-    private $APELIDO1;
-    private $APELLIDO2;
+    private $APELLIDOS;
     private $USUARIO;
     private $CONTRASEÑA;
     private $CORREO;
@@ -13,17 +12,15 @@ class Miembro_Model{
      * Miembro_Model constructor.
      * @param $DNI
      * @param $NOMBRE
-     * @param $APELLIDO1
-     * @param $APELLIDO2
+     * @param $APELLIDOS
      * @param $USUARIO
      * @param $CONTRASEÑA
      * @param $CORREO
      */
-    public function __construct($DNI=NULL, $NOMBRE=NULL, $APELLIDO1=NULL, $APELLIDO2=NULL, $USUARIO=NULL, $CONTRASEÑA=NULL, $CORREO=NULL){
+    public function __construct($DNI=NULL, $NOMBRE=NULL, $APELLIDOS=NULL, $USUARIO=NULL, $CONTRASEÑA=NULL, $CORREO=NULL){
         $this->DNI = $DNI;
         $this->NOMBRE = $NOMBRE;
-        $this->APELIDO1 = $APELLIDO1;
-        $this->APELLIDO2 = $APELLIDO2;
+        $this->APELLIDOS = $APELLIDOS;
         $this->USUARIO = $USUARIO;
         $this->CONTRASEÑA = $CONTRASEÑA;
         $this->CORREO = $CORREO;
@@ -49,21 +46,12 @@ class Miembro_Model{
     }
 
 
-    public function getApellido1(){
-        return $this->APELIDO1;
+    public function getApellidos(){
+        return $this->APELLIDOS;
     }
 
-    public function setApellido1($apellido1){
-        $this->APELIDO1 = $apellido1;
-    }
-
-
-    public function getApellido2(){
-        return $this->APELLIDO2;
-    }
-
-    public function setApellido2($apellido2){
-        $this->APELLIDO2 = $apellido2;
+    public function setApellidos($apellidos){
+        $this->APELLIDOS = $apellidos;
     }
 
 
