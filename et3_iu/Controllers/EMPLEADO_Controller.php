0da5030316109eccb581d31c22c0a3a9bd9ef77a
Barajas04 <isma.verin@gmail.com>
2016-12-29 13:11:56 +0100

    empleado actualizado

--- et3_iu/Controllers/EMPLEADO_Controller.php
+++ et3_iu/Controllers/EMPLEADO_Controller.php
@@ -25,43 +25,15 @@ function get_data_form(){
 
 	$EMP_USER=$_REQUEST['EMP_USER'];
 	$EMP_PASSWORD=$_REQUEST['EMP_PASSWORD'];
-	$EMP_CUENTA=$_REQUEST['EMP_CUENTA'];
 	$EMP_TIPO=ConsultarIDRol($_REQUEST['EMP_TIPO']);
-
 	$EMP_ESTADO=$_REQUEST['EMP_ESTADO'];
-	$EMP_DNI = $_REQUEST['EMP_DNI'];
 	$EMP_EMAIL = $_REQUEST['EMP_EMAIL'];
-	$EMP_FECH_NAC = $_REQUEST['EMP_FECH_NAC'];
-	$EMP_DIRECCION = $_REQUEST['EMP_DIRECCION'];
 	$EMP_NOMBRE = $_REQUEST['EMP_NOMBRE'];
 	$EMP_APELLIDO = $_REQUEST['EMP_APELLIDO'];
-	$EMP_TELEFONO = $_REQUEST['EMP_TELEFONO'];
-	$EMP_COMENTARIOS = $_REQUEST['EMP_COMENTARIOS'];
 
-//Si no se ha introducido un nuevo archivo se deja el que había
-	if (isset($_FILES['EMP_FOTO']['name']) && ($_FILES['EMP_FOTO']['name']!=='')) {
-
-		$EMP_FOTO = '../Documents/Empleados/' . $_REQUEST['EMP_DNI'] . '/Foto/' . $_FILES['EMP_FOTO']['name'];
-
-	}
-	else {
-
-		$EMP_FOTO='';
-
-	}
-	if (isset($_FILES['EMP_NOMINA']['name']) && ($_FILES['EMP_NOMINA']['name']!=='')) {
-
-
-		$EMP_NOMINA = '../Documents/Empleados/' . $_REQUEST['EMP_DNI'] . '/Nomina/' . $_FILES['EMP_NOMINA']['name'];
-	}
-	else {
-
-
-		$EMP_NOMINA='';
-	}
 	$accion = $_REQUEST['accion'];
 //crea el empleado con los datos anteriores
-	$usuario = new EMPLEADOS_Modelo($EMP_USER, $EMP_PASSWORD, $EMP_FECH_NAC, $EMP_EMAIL, $EMP_NOMBRE, $EMP_APELLIDO, $EMP_DNI, $EMP_TELEFONO, $EMP_CUENTA, $EMP_DIRECCION, $EMP_COMENTARIOS, $EMP_TIPO, $EMP_ESTADO,$EMP_FOTO,$EMP_NOMINA);
+	$usuario = new EMPLEADOS_Modelo($EMP_USER, $EMP_PASSWORD, $EMP_EMAIL, $EMP_NOMBRE, $EMP_APELLIDO,$EMP_TIPO, $EMP_ESTADO);
 
 	return $usuario;
 }
@@ -81,30 +53,10 @@ if (!isset($_REQUEST['accion'])){
 				}
 			}
 			else{
-
-
 				$_REQUEST['EMP_ESTADO']='Activo'; //Siempre que se inserta estará activo en un principio
 				$usuario = get_data_form();
-				//Creamos las carpetas para guardar los archivos
-				$carpetaFoto='../Documents/Empleados/'.$_REQUEST['EMP_DNI'].'/Foto/';
-				$carpetaNomina='../Documents/Empleados/'.$_REQUEST['EMP_DNI'].'/Nomina/';
 
-				if($_FILES['EMP_FOTO']['name']!=='') {
-					if (!file_exists($carpetaFoto)) {
-						mkdir($carpetaFoto, 0777, true);
-					}
-
-					move_uploaded_file($_FILES['EMP_FOTO']['tmp_name'], $carpetaFoto . $_FILES['EMP_FOTO']['name']);
-				}
-				if($_FILES['EMP_NOMINA']['name']!=='') {
-					if (!file_exists($carpetaNomina)) {
-						mkdir($carpetaNomina, 0777, true);
-					}
-
-
-					move_uploaded_file($_FILES['EMP_NOMINA']['tmp_name'], $carpetaNomina . $_FILES['EMP_NOMINA']['name']);
-
-				} //Insertamos el usuario
+				//Insertamos el usuario
 				$respuesta = $usuario->Insertar();
 				new Mensaje($respuesta, 'EMPLEADO_Controller.php');
 			}
@@ -122,9 +74,9 @@ if (!isset($_REQUEST['accion'])){
 					new EMPLEADOS_Borrar($valores, 'EMPLEADO_Controller.php');
 				}
 			}
-			else{ //Estos campos no se muestran en el formulario de borrado por lo que se ponen vacíos
+			else{
+				//Estos campos no se muestran en el formulario de borrado por lo que se ponen vacíos
 				$_REQUEST['EMP_PASSWORD']='';
-
 				$_REQUEST['EMP_ESTADO']='';
 				$_REQUEST['EMP_TIPO']='';
 				$usuario = get_data_form();
@@ -133,9 +85,7 @@ if (!isset($_REQUEST['accion'])){
 			}
 			break;
 		case  $strings['Modificar']:
-
-
-			if (!isset($_REQUEST['EMP_DNI'])){
+			if (!isset($_REQUEST['EMP_PASSWORD'])){
 				//Crea un usuario solo con el user para posteriormente rellenar el formulario con sus datos
 				$usuario = new EMPLEADOS_Modelo($_REQUEST['EMP_USER'], '', '', '', '', '', '', '', '','','', '', '', '', '');
 				$valores = $usuario->RellenaDatos($_REQUEST['EMP_USER']);
@@ -149,28 +99,7 @@ if (!isset($_REQUEST['accion'])){
 			}
 			else{
 				$_REQUEST['EMP_TIPO']='';
-
 				$usuario = get_data_form();
-				$carpetaFoto='../Documents/Empleados/'.$_REQUEST['EMP_DNI'].'/Foto/';
-				$carpetaNomina='../Documents/Empleados/'.$_REQUEST['EMP_DNI'].'/Nomina/';
-
-					//Se realizan las modificaciones también en las carpetas de documentos
-				if($_FILES['EMP_FOTO']['name']!=='') {
-					if (!file_exists($carpetaFoto)) {
-						mkdir($carpetaFoto, 0777, true);
-					}
-
-					move_uploaded_file($_FILES['EMP_FOTO']['tmp_name'], $carpetaFoto . $_FILES['EMP_FOTO']['name']);
-				}
-				if($_FILES['EMP_NOMINA']['name']!=='') {
-					if (!file_exists($carpetaNomina)) {
-						mkdir($carpetaNomina, 0777, true);
-					}
-
-
-					move_uploaded_file($_FILES['EMP_NOMINA']['tmp_name'], $carpetaNomina . $_FILES['EMP_NOMINA']['name']);
-
-				}
 				$respuesta = $usuario->Modificar();
 				new Mensaje($respuesta, 'EMPLEADO_Controller.php');
 			}
@@ -185,28 +114,14 @@ if (!isset($_REQUEST['accion'])){
 				}
 			}
 			else{
-
-//Establecemos a cadena vacía la información que no se obtiene del formulario
-			$_REQUEST['EMP_CUENTA']='';
+				//Establecemos a cadena vacía la información que no se obtiene del formulario
+				$_REQUEST['EMP_CUENTA']='';
 				$_REQUEST['EMP_TIPO']='';
-			$_REQUEST['EMP_ESTADO']='';
-
-		$_REQUEST['EMP_EMAIL']='';
-				$_REQUEST['EMP_FECH_NAC']='';
-				$_REQUEST['EMP_DIRECCION']='';
-
-
-			$_REQUEST['EMP_TELEFONO']='';
+				$_REQUEST['EMP_ESTADO']='';
+				$_REQUEST['EMP_EMAIL']='';
 				$_REQUEST['EMP_PASSWORD']='';
-		$_REQUEST['EMP_COMENTARIOS']='';
-			;
-				$_REQUEST['EMP_FOTO']='';
-				$_REQUEST['EMP_NOMINA']='';
-
 				$usuario = get_data_form();
 				$datos = $usuario->Consultar();
-
-
 				new EMPLEADOS_ShowConsulta($datos, 'EMPLEADO_Controller.php');
 			}
 			break;
