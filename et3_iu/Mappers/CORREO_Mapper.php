454170e842e7985de2e7319c21caba83dcc1adef
jnseoane <jnseoane@esei.uvigo.es>
2016-12-30 13:17:14 +0100

    metodo enviados

--- et3_iu/Mappers/CORREO_Mapper.php
+++ et3_iu/Mappers/CORREO_Mapper.php
@@ -12,6 +12,19 @@ class NotificacionMapper {
       }
   }
 
-
+  function listarEnviadas()
+  {
+    $correo = array();
+    $this ->conectarBD();
+    $sql = "SELECT * FROM CORREO WHERE EMISOR = '" .$_SESSION['login']. "' ORDER BY FECHAENVIO";
+    if($resultado = $this->mysqli->query($sql)){
+      $aux=$resultado->num_rows;
+      while($aux>0){
+        $correo[$resultado->num_rows - $aux] = $resultado->fetch_array();
+        $aux = $aux -1;
+      }
+      return $correo;
+    }
+  }
 
   }
