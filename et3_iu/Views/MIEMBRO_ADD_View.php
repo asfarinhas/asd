a9f11b48ee60e3f98343f09a09dd9330a60d9c6e
Raquel Marcos <raquelmarcosenr@gmail.com>
2017-01-02 17:04:21 +0100

    Terminado

--- et3_iu/Views/MIEMBRO_ADD_View.php
+++ et3_iu/Views/MIEMBRO_ADD_View.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Created by PhpStorm.
  * User: RaquelMarcos
@@ -7,32 +8,57 @@
 
 class MiembroAddView{
 
-    <form action="MIEMBRO_Controller.php" name ="formAddMiembro" enctype="multipart/form-data" method="post" onsubmit=" " >
+function __construct(){
+    $this->showView();
+}
+
+
+function showView(){
+
+    $strings = array();
+    include '../Locates/Strings_' . $_SESSION['IDIOMA'] . '.php';
+
+?>
+<head>
+    <link rel="stylesheet" href="../Styles/styles.css" type="text/css" media="screen"/>
+
+</head>
+<div>
+    <h1><span class="form-title"><?=$strings['ModificarMiembro']?></h1>
+
+    <form action="../Controllers/MIEMBRO_Controller.php" name="formAddMiembro" enctype="multipart/form-data" method="post" onsubmit=" ">
+
 
         <div>
-            <?//= $strings['Nombre']?> <br/>
-            <input type="text" name="NOMBRE" value="<?= $miembro->getNombre()?>" placeholder="<?//= $strings['Nombre']?>"  id="nombre" required ><br/>
+            <?= $strings['Nombre']?> <br/>
+            <input type="text" name="nombre" placeholder="<?= $strings['Nombre']?>"  id="nombre" required ><br/>
         </div>
 
         <div>
-            <?//= $strings['Apellidos']?> <br/>
-            <input type="text" name="APELLIDOS" value="<?= $miembro->getApellidos()?>" placeholder="<?//= $strings['Apellidos']?>"  id="apellidos" required ><br/>
+            <?= $strings['EMP_APELLIDO']?> <br/>
+            <input type="text" name="apellidos" placeholder="<?= $strings['EMP_APELLIDO']?>"  id="apellidos" required ><br/>
         </div>
 
         <div>
-            <?//= $strings['Usuario']?> <br/>
-            <input type="date" name="USUARIO" value="<?= $miembro->getUsuario()?>" placeholder="<?//= $strings['Usuario']?>"  id="usuario" required ><br/>
+            <?= $strings['EMP_USER']?> <br/>
+            <input type="text" name="usuario" placeholder="<?= $strings['EMP_USER']?>"  id="usuario" required ><br/>
         </div>
 
         <div>
-            <?//= $strings['Contraseña']?> <br/>
-            <input type="date" name="CONTRASEÑA" value="<?= $miembro->getContraseña()?>" placeholder="<?//= $strings['Contraseña']?>"  id="contraseña" required ><br/>
+            <?= $strings['EMP_PASSWORD']?> <br/>
+            <input type="password" name="password"  placeholder="<?= $strings['EMP_PASSWORD']?>"  id="password" required ><br/>
         </div>
 
         <div>
-            <?//= $strings['Correo']?> <br/>
-            <input type="date" name="CORREO" value="<?= $miembro->getCorreo()?>" placeholder="<?//= $strings['Correo']?>"  id="correo" required ><br/>
+            <?= $strings['EMP_EMAIL']?> <br/>
+            <input type="email" name="correo" placeholder="ej: aaaa@aaaa.aa"  id="correo" required ><br/>
         </div>
 
-        <input type="submit" name="accion" value="ADD"><?//= $strings['Add'] ?>
+        <input type="hidden"  name="accion" value="add">
+        <input type='submit' onclick="DoSubmit()" value="<?=$strings['Insertar'] ?>"><br/><br/>
+    </form>
+    <a class="form-link" href='../Controllers/MIEMBRO_Controller.php'><?=$strings['Volver']?> </a>
+</div>
+<?php
+}
 }
