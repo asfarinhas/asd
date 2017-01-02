580ca7bf4783c6dc5044237a1a42f0630977cc37
cgdelgado <cgdelgado@esei.uvigo.es>
2017-01-02 11:32:49 +0100

    miembro edit

--- et3_iu/Views/MIEMBRO_EDIT_View.php
+++ et3_iu/Views/MIEMBRO_EDIT_View.php
@@ -23,38 +23,50 @@ class MiembroEditView{
     function showView(){
 
         $miembro = $this->getDatos();
+        $strings = array();
+        include '../Locates/Strings_' . $_SESSION['IDIOMA'] . '.php';
 
-?>
+        ?>
+        <head>
+            <link rel="stylesheet" href="../Styles/styles.css" type="text/css" media="screen"/>
 
-<form action="MIEMBRO_Controller.php" name="formEditMiembro" enctype="multipart/form-data" method="post" onsubmit=" ">
+        </head>
+        <div>
+            <h1><span class="form-title"><?=$strings['ModificarMiembro']?></h1>
+
+            <form action="../Controllers/MIEMBRO_Controller.php" name="formEditMiembro" enctype="multipart/form-data" method="post" onsubmit=" ">
 
 
-    <div>
-        <?//= $strings['nombre']?> <br/>
-        <input type="text" name="NOMBRE" value="<?= $miembro->getNombre()?>" placeholder="<?//= $strings['nombre']?>"  id="nombre" required ><br/>
-    </div>
+                <div>
+                    <?= $strings['Nombre']?> <br/>
+                    <input type="text" name="NOMBRE" value="<?= $miembro->getNombre()?>" placeholder="<?= $strings['Nombre']?>"  id="nombre" required ><br/>
+                </div>
 
-    <div>
-        <?//= $strings['apellidos']?> <br/>
-        <input type="text" name="APELLIDOS" value="<?= $miembro->getApellidos()?>" placeholder="<?//= $strings['Descripcion']?>"  id="apellidos" required ><br/>
-    </div>
+                <div>
+                    <?= $strings['EMP_APELLIDO']?> <br/>
+                    <input type="text" name="APELLIDOS" value="<?= $miembro->getApellidos()?>" placeholder="<?= $strings['EMP_APELLIDO']?>"  id="apellidos" required ><br/>
+                </div>
 
-    <div>
-        <?//= $strings['usuario']?> <br/>
-        <input type="date" name="USUARIO" value="<?= $miembro->getUsuario()?>" placeholder="<?//= $strings['usuario']?>"  id="usuario" required ><br/>
-    </div>
+                <div>
+                    <?= $strings['EMP_USER']?> <br/>
+                    <input type="date" name="USUARIO" value="<?= $miembro->getUsuario()?>" placeholder="<?= $strings['EMP_USER']?>"  id="usuario" required ><br/>
+                </div>
 
-    <div>
-        <?//= $strings['contraseña']?> <br/>
-        <input type="date" name="CONTRASEÑA" value="<?= $miembro->getContraseña()?>" placeholder="<?//= $strings['usuario']?>"  id="contraseña" required ><br/>
-    </div>
+                <div>
+                    <?= $strings['EMP_PASSWORD']?> <br/>
+                    <input type="date" name="CONTRASEÑA" value="<?= $miembro->getContraseña()?>" placeholder="<?= $strings['EMP_PASSWORD']?>"  id="password" required ><br/>
+                </div>
 
-    <div>
-        <?//= $strings['correo']?> <br/>
-        <input type="date" name="CORREO" value="<?= $miembro->getCorreo()?>" placeholder="<?//= $strings['usuario']?>"  id="correo" required ><br/>
-    </div>
+                <div>
+                    <?= $strings['EMP_EMAIL']?> <br/>
+                    <input type="date" name="CORREO" value="<?= $miembro->getCorreo()?>" placeholder="ej: aaaa@aaaa.aa"  id="correo" required ><br/>
+                </div>
 
-    <input type="submit" name="accion" value="edit"><?//= $strings['Edit'] ?>
+                <input type="hidden"  name="accion" value="edit">
+                <input type='submit' onclick="DoSubmit()" value="<?=$strings['Modificar'] ?>"><br/><br/>
+            </form>
+            <a class="form-link" href='../Controllers/MIEMBRO_Controller.php'><?=$strings['Volver']?> </a>
+        </div>
   <?php
     }
 }
