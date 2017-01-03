dbe6a5f730d2439e06473f00febd7f68241034fd
iaquintas <iaquintas@esei.uvigo.es>
2017-01-03 13:28:30 +0100

    Correcion de indicador

--- et3_iu/Views/DEFAULT_Vista.php
+++ et3_iu/Views/DEFAULT_Vista.php
@@ -34,7 +34,7 @@
 
             <ul>
               <?php $noti=new NotificacionMapper(); ?>
-                <li><a href="../Controllers/NOTIFICACION_Controller.php"><?php echo 'Notificaciones Pendientes:'.count($noti->listarRecibidas()); ?></a></li>
+                <li><a href="../Controllers/NOTIFICACION_Controller.php"><?php echo 'Notificaciones Pendientes:'.count($noti->buscarNoLeidas()); ?></a></li>
                 <li><a href="../Functions/Desconectar.php"><?php echo  $strings['Cerrar Sesión']; ?></a></li>
                 <li> <?php echo $strings['Usuario'].": ". $_SESSION['login']; ?> </li>
 
