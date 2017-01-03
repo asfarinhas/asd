7f81af3a925155414e8970d5d3dbcd733b6e89d6
abhermida <abhermida@esei.uvigo.es>
2017-01-03 13:59:32 +0100

    Añadido al menú tarea, entregable y perfil

--- et3_iu/Functions/LibraryFunctions.php
+++ et3_iu/Functions/LibraryFunctions.php
@@ -1485,6 +1485,12 @@ function añadirFuncionalidades($NOM) {
                 case "GESTION PERFIL":
                         ?><a style="font-size:20px;" href='../Controllers/MIEMBRO_Controller.php'><?php echo $strings['Gestión de Perfil'] ?></a><br><br> <?php
                         break;
+                case "GESTION TAREAS":
+                    ?><a style="font-size:20px;" href='../Controllers/TAREA_Controller.php'><?php echo $strings['Gestión de Tareas'] ?></a><br><br> <?php
+                    break;
+                case "GESTION ENTREGABLES":
+                    ?><a style="font-size:20px;" href='../Controllers/ENTREGABLE_Controller.php'><?php echo $strings['Gestión de Entregables'] ?></a><br><br> <?php
+                    break;
                 default:
                     $link = str_replace(" ", "_", ConsultarNOMFuncionalidad($fila['FUNCIONALIDAD_ID'])) . "_Controller.php";
                     echo "<a style='font-size:20px;'href='../Controllers/" . $link . "'>" . ConsultarNOMFuncionalidad($fila['FUNCIONALIDAD_ID']) . " </a><br><br>";
