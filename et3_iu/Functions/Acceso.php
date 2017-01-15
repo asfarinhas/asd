10ff1514ff6d99bdf8cf884c8562991487fc9f8a
Barajas04 <isma.verin@gmail.com>
2017-01-15 12:55:12 +0100

    Arreglando Registro

--- et3_iu/Functions/Acceso.php
+++ et3_iu/Functions/Acceso.php
@@ -3,9 +3,10 @@
 	include '../Views/MIEMBRO_ADD_View.php';
 	if (isset($_REQUEST['accion'])){
 
-
 		if ($_REQUEST['accion'] == 'Registro'){
-            header("Location:../Controllers/MIEMBRO_Controller.php");
+			session_start();
+            $_SESSION['IDIOMA'] = $_REQUEST["IDIOMA"];
+            new MiembroAddVista();
             die();
         }
 
